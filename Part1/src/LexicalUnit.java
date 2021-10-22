28ac42cfebab93e33e0af34944e358a9c39c1b0a
faeschier <nikita.faesch@yahoo.fr>
2021-10-22 13:12:46 +0200

    rectif

---
+++ Part1/src/LexicalUnit.java
@@ -0,0 +1,33 @@
+public enum LexicalUnit{
+    VARNAME, //fait	
+    NUMBER, //fait
+    BEG,	//fait
+    END,	//fait
+    SEMICOLON,	//fait
+    ASSIGN,	//fait
+    LPAREN,	//fait
+    RPAREN,	//fait
+    MINUS,	//fait
+    PLUS,	//fait
+    TIMES,	//fait 
+    DIVIDE,	//fait
+    IF,	//fait
+    THEN,	//fait
+    ENDIF,
+    ELSE,	//fait
+    NOT,	//fait
+    EQUAL,	//fait
+    GREATER,	//fait
+    SMALLER,	//fait
+    WHILE,	//fait
+    DO,	//fait
+    ENDWHILE,	
+    FOR,	//fait
+    FROM,	//fait
+    BY,	//fait
+    TO,
+    ENDFOR,	//fait
+    PRINT,	//fait
+    READ,	//fait
+	END_OF_STREAM
+}
