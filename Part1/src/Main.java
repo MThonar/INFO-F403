2c7ad2ae3f469da9a1e1c3ed2f4d4fa8227fe9d0
faeschier <nikita.faesch@yahoo.fr>
2021-10-22 12:52:53 +0200

    proj

--- Part1/src/Main.java
+++ Part1/src/Main.java
@@ -3,24 +3,25 @@ import java.io.*;
 
 
 class Main {
-	public static void main (String[] args)
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 	{
    		FileReader source = new FileReader(args [0]);
    		final LexicalAnalyzer analyzer = new LexicalAnalyzer(source);
    		ArrayList<String> ls = new ArrayList<String>();
    		while(true) {
 	   		Symbol s = analyzer.nextToken();
-	   		System.out.println(s.toString());
 	   		if (s.getType().toString().equals("VARNAME")) {
+	   			System.out.println(s.toString());
 	   			ls.add(s + " " + s.getValue());
 	   		}	
-	   		else if(s.getType().toString() == "EOF") {
+	   		else if(s.getType().toString() == "END") {
 	   			System.out.println("Variables");
 	   			for(int i = 0; i < ls.size(); i++) {
+	   			
 		    			System.out.println(ls.get(i));
 	   			}
 	   			break;
    			}
 	 	}
 	}
-}
+}}
