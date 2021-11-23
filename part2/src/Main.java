8a67253744db10a44c281bc0fe120e53dfeeee1c
mthonar <maxime.thonar@gmail.com>
2021-11-23 10:44:21 +0100

    If condition in main

--- part2/src/Main.java
+++ part2/src/Main.java
@@ -23,9 +23,6 @@ class Main {
                 if(s.getType().toString() == "END") {
                 	list.add(s);
                 }
-                /*for (Symbol i : list){
-		        System.out.println(i);
-		 }*/
                 Parser parser = new Parser(list);
                 String str = parser.startParsing();
                 writer.write(str);
@@ -38,9 +35,6 @@ class Main {
         }
         else if(args.length ==1) {
         	try {
-            	//System.out.println(args.length);
-            	for (String i:args){
-            		System.out.println(i);}
                 final FileReader source=new FileReader(args [0]);
                 final LexicalAnalyzer lexAn = new LexicalAnalyzer(source);
                 Symbol s = lexAn.nextToken();
@@ -51,9 +45,6 @@ class Main {
                 if(s.getType().toString() == "END") {
                 	list.add(s);
                 }
-                /*for (Symbol i : list){
-		        System.out.println(i);
-		 }*/
                 Parser parser = new Parser(list);
                 parser.startParsing();
             }
