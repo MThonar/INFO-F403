6b49425037dc1bfcef7da9fce204aec86b2baa49
mthonar <maxime.thonar@gmail.com>
2021-12-20 16:05:44 +0100

    LLVMprinter class

---
+++ part3/src/LLVMprinter.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class LLVMprinter {
+    private ArrayList<Symbol> AST;
+
+    public LLVMprinter(ArrayList<Symbol> AST){
+        this.AST = AST;
+    }
+
+    public void program(){
+
+    }
+
+    public void cond(){
+
+    }
+
+    public void exprArith(){
+
+    }
+
+    public void assign(){
+
+    }
+
+    public void If(){
+
+    }
+
+    public void While(){
+
+    }
+
+    public void For(){
+
+    }
+
+    public void print(){
+
+    }
+
+    public void read(){
+
+    }
+
+}
