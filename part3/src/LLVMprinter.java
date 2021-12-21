2ea69443df463ea2acda8f4f99ef2073d3f4dd1a
mthonar <maxime.thonar@gmail.com>
2021-12-21 23:32:45 +0100

    Multiple counters

--- part3/src/LLVMprinter.java
+++ part3/src/LLVMprinter.java
@@ -152,8 +152,8 @@ public class LLVMprinter {
             globalIncrement++;
             plusIncrement++;
             intermediateIncrement++;
-            codeFragment += "%" + globalIncrement + " = add i32 %" + globalIncrement +
-                    ",%" + globalIncrement + "\n";
+            codeFragment += "%" + globalIncrement + " = add i32 %" + (globalIncrement-2) +
+                    ",%" + (globalIncrement-1) + "\n";
         }
         return codeFragment;
     }
