a29c9b6296434a279ea2c0402e34558839d9a5c5
mthonar <maxime.thonar@gmail.com>
2021-12-21 14:30:19 +0100

    LLVMprinter class

--- part3/src/LLVMprinter.java
+++ part3/src/LLVMprinter.java
@@ -107,7 +107,7 @@ public class LLVMprinter {
                     intermediateIncrement + "\n";
             intermediateIncrement++;
             codeFragment += "%intermediate" + intermediateIncrement + " = alloca i32\nstore i32 " +
-                    rightTree + ", i32* intermediate" + intermediateIncrement + "\n%" +
+                    rightTree + ", i32* %intermediate" + intermediateIncrement + "\n%" +
                     globalIncrement + " = load i32, i32* %intermediate" +
                     (intermediateIncrement-1) + "\n";
             globalIncrement++;
