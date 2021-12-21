a1634381d4fcf143d4ac24039f82f0a1e0d483f4
mthonar <maxime.thonar@gmail.com>
2021-12-21 16:27:24 +0100

    Recursive plus

--- part3/src/LLVMprinter.java
+++ part3/src/LLVMprinter.java
@@ -111,8 +111,8 @@ public class LLVMprinter {
         String leftTree = "";
         String rightTree = "";
         System.out.println(!(isAnOperator(exprArith.get(1))));
-        System.out.println((exprArith.get(2).getValue() == LexicalUnit.PLUS));
-        if( !(isAnOperator(exprArith.get(1))) && (exprArith.get(2).getValue() == LexicalUnit.PLUS) ){
+        System.out.println((exprArith.get(2).getType() == LexicalUnit.PLUS));
+        if( !(isAnOperator(exprArith.get(1))) && (exprArith.get(2).getType() == LexicalUnit.PLUS) ){
             System.out.println("dans le if");
             leftTree = exprArith.get(1).getValue().toString();
             ArrayList<Symbol> newExprArith = new ArrayList<>();
