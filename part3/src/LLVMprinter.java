17a8aceb5ab2691b8434032ef87bc5f269b55b79
mthonar <maxime.thonar@gmail.com>
2021-12-22 01:47:25 +0100

    Left times right plus

--- part3/src/LLVMprinter.java
+++ part3/src/LLVMprinter.java
@@ -192,8 +192,8 @@ public class LLVMprinter {
         }
         else{
             System.out.println("ON EST PAS DEDANS");
-            System.out.println((exprArith.get(1).getValue() == LexicalUnit.TIMES));
-            System.out.println((exprArith.get(4).getValue() == LexicalUnit.PLUS));
+            System.out.println((exprArith.get(1).getType() == LexicalUnit.TIMES));
+            System.out.println((exprArith.get(4).getType() == LexicalUnit.PLUS));
         }
         return codeFragment;
     }
